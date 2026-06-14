e5e6782e51f0653c614d0dd707700dc5101a3230
lukaMis <luka.mis@gmail.com>
2026-06-14 22:12:02 +0200

    added game time UI that shows game time

--- scripts/game_time_display.gd
+++ scripts/game_time_display.gd
@@ -4,9 +4,10 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	game_time_display_label.text = 'Game Time'
 	TimeEngine.time_formatted_updated.connect(_on_game_time_update)
 # END
 
 func _on_game_time_update(time_update:String)->void:
-	game_time_display_label.text = time_update
+	game_time_display_label.text = 'Game Time:' + ' ' + time_update
 # END
